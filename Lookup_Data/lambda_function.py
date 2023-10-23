5396adf611c56eb586069b7efd5871863ece9836
Sagar-NucleusTeq <sagar.kalthiya@nucleusteq.com>
2023-10-24 01:40:05 +0530

    lambda_with_minimal_permission: test3

--- Lookup_Data/lambda_function.py
+++ Lookup_Data/lambda_function.py
@@ -54,7 +54,6 @@ def lambda_handler(event, context):
         print(df)
         print(df.count())
         print("Parquet file read into Pandas DataFrame.")
-        print("test .")
 
         # Convert all columns to strings
         df = df.astype(str)
